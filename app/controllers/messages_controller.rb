1d3dcd8de75b06c79ec699fefab95152c792b17b
Chenyang Lei <leichenyang@gmail.com>
2013-05-07 03:08:08 -0400

    Pass all test cases but the last one

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,13 @@
 class MessagesController < ApplicationController
   def create
     message = Message.new
-    message.text = params[:messages][:text]
+    message.text = params[:message][:text]
     message.user = User.find_by_name(params[:user_name])
 
     if (message.save)
-      render :json => {}.to_json, :status => 200
+      render :json => message.to_json, :status => 201
     else
-      render :status => 500
+      render :status => 422
     end
 
   end
@@ -15,9 +15,9 @@ class MessagesController < ApplicationController
   def index
     last_message = params[:last_message].to_i
     if(last_message.nil?)
-      render :json => Message.all.to_json, :status=>200
+      render :json => Message.all.to_json, :status=>201
     else
-      render :json => Message.all(:conditions => ["id > ?",last_message]), :status=>200
+      render :json => Message.all(:conditions => ["id > ?",last_message]), :status=>201
     end
   end
 end
